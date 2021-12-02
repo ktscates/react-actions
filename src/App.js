c2e3349a7afb0fbb639f28dce2fe6bdf1272fb38
ktscates <cates2506@gmail.com>
2021-12-02 17:52:24 +0200

    deploying to prod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload pipeline success.
+          Edit <code>src/App.js</code> and save to reload, is it working?
         </p>
         <a
           className="App-link"
